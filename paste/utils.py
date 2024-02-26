7a59f43431981b37c1749b8ff30675a9e9b74587
shabane <m.mohamadshabane@gmail.com>
2024-02-27 00:03:25 +0330

    feat: write functions to save the file and use it in view

---
+++ paste/utils.py
@@ -0,0 +1,40 @@
+from .models import File, Limit, Username
+from hashlib import md5
+
+
+def name_it(file) -> str:
+    file_data = file.file.read()
+    file_type = file.name.split('.')[-1]
+    return md5(file_data).hexdigest() + f'.{file_type}'
+
+#TODO: make 24 dynamic
+def save(file, name: str, password: str, username: str = None, t_limit: int = 24, v_limit: int = 0):
+    if password:
+        new_file = File(name=name, file=file, password=password)
+        new_file.save()
+        Limit(file=new_file, time=t_limit, view=v_limit).save()
+        if username:
+            Username(file=new_file, name=username).save()
+    else:
+        new_file = File(name=name, file=file)
+        new_file.save()
+        Limit(file=new_file, time=t_limit, view=v_limit).save()
+        if username:
+            Username(file=new_file, name=username).save()
+
+
+def interpret(request):
+    if request.FILES.dict().get('file'):
+        file = request.FILES.get('file')
+        name = name_it(file)
+        save(file, name, request.GET.get('password'))
+    elif request.FILES.dict().get('once'):
+        file = request.FILES.get('once')
+        name = name_it(file)
+        save(file, name, request.GET.get('password'), v_limit=1)
+    else:
+        #TODO: split username and other interprets
+        username = list(request.FILES.keys())[0]
+        file = request.FILES.get(username)
+        name = name_it(file)
+        save(file, name, request.GET.get('password'), username=username)
--- paste/views.py
+++ paste/views.py
@@ -3,16 +3,13 @@ from django.views.decorators.csrf import csrf_exempt
 from curlpaste.settings import MEDIA_ROOT
 from hashlib import md5
 import os.path
-
+from .utils import interpret
 
 @csrf_exempt
 def root(request):
     if request.method == 'POST':
-        file_data = request.FILES.get('file').file.read()
-        file_type = request.FILES.get('file').name.split('.')[-1]
-        file_name = os.path.join(md5(file_data).hexdigest()+f'.{file_type}')
-        with open(os.path.join(MEDIA_ROOT, file_name), 'wb') as fli:
-            fli.write(file_data)
+        print()
+        interpret(request)
         return HttpResponse('file saved')
     elif request.method == 'GET':
         return HttpResponse('by')

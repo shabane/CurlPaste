eb3292a1cd4cace4fbc302115057270072bb24be
shabane <m.mohamadshabane@gmail.com>
2024-02-27 10:47:56 +0330

    feat: return the file name in db

--- paste/models.py
+++ paste/models.py
@@ -8,13 +8,22 @@ class File(models.Model):
     visited = models.IntegerField(default=0)
     password = models.CharField(max_length=1024, null=True, blank=False)
 
+    def __str__(self):
+        return str(self.file)
+
 
 class Username(models.Model):
     name = models.CharField(max_length=255)
     file = models.ForeignKey(File, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 
 class Limit(models.Model):
     view = models.IntegerField()
     time = models.IntegerField()
     file = models.ForeignKey(File, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'view: {self.view}, time: {self.time}, file: {self.file.name}'

ff1d91a07f6469715804b1b8d95299e7f8fc0440
shabane <m.mohamadshabane@gmail.com>
2024-02-27 13:27:24 +0330

    feat: return file url or username of sender

--- paste/utils.py
+++ paste/utils.py
@@ -8,33 +8,36 @@ def name_it(file) -> str:
     return md5(file_data).hexdigest() + f'.{file_type}'
 
 #TODO: make 24 dynamic
-def save(file, name: str, password: str, username: str = None, t_limit: int = 24, v_limit: int = 0):
+def save(file, name: str, password: str, username: str = None, t_limit: int = 24, v_limit: int = 0) -> str:
     if password:
         new_file = File(name=name, file=file, password=password)
         new_file.save()
         Limit(file=new_file, time=t_limit, view=v_limit).save()
         if username:
             Username(file=new_file, name=username).save()
+        return new_file
     else:
         new_file = File(name=name, file=file)
         new_file.save()
         Limit(file=new_file, time=t_limit, view=v_limit).save()
         if username:
             Username(file=new_file, name=username).save()
+        return new_file
 
 
 def interpret(request):
     if request.FILES.dict().get('file'):
         file = request.FILES.get('file')
         name = name_it(file)
-        save(file, name, request.GET.get('password'))
+        return save(file, name, request.GET.get('password'))
     elif request.FILES.dict().get('once'):
         file = request.FILES.get('once')
         name = name_it(file)
-        save(file, name, request.GET.get('password'), v_limit=1)
+        return save(file, name, request.GET.get('password'), v_limit=1)
     else:
         #TODO: split username and other interprets
         username = list(request.FILES.keys())[0]
         file = request.FILES.get(username)
         name = name_it(file)
         save(file, name, request.GET.get('password'), username=username)
+        return username

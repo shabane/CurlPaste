53a8cb0e20399cb1a67033fadf1fe2a0ac070a88
shabane <m.mohamadshabane@gmail.com>
2024-02-28 22:34:59 +0330

    maintain: allow all the host

--- curlpaste/settings.py
+++ curlpaste/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-wvv9v03r0gcs-cflw-1eokyrb=2@0n(yh*%on740jrk6ofy(wm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

c83095682772f90f490141de4530f7f2d0c5ac7b
shabane <m.mohamadshabane@gmail.com>
2024-03-03 00:45:37 +0330

    feat: delete file that visited or time exceeded:
    	create two seprate function for delete file and
    	deleting the row in db,
    	make the watcher run in a infinite loop

--- watcher.py
+++ watcher.py
@@ -1,25 +1,40 @@
 #!/usr/bin/env python3
 import sqlite3
 from datetime import datetime, timedelta
+import os
 
 
-db = sqlite3.connect('db.sqlite3')
-# id, view, time, file
-limits = db.execute('select * from paste_limit').fetchmany()
+def remove_file(path: str):
+    os.remove(path)
 
-# id, name, file, date_time, visited, password
-files = db.execute('select * from paste_file').fetchmany()
+def remove_from_db(db, id: int):
+    cursor = db.cursor()
+    cursor.execute(f"DELETE from paste_file where id = {id}")
+    cursor.execute(f"DELETE from paste_limit where id = {id}")
+    db.commit()
 
-for file, limit in zip(files, limits):
-    _file_time = file[3]
-    _file_time = datetime.strptime(_file_time, "%Y-%m-%d %H:%M:%S.%f")
-    _limit_hour = limit[2]
+while True:
+    db = sqlite3.connect('db.sqlite3')
+    # id, view, time, file
+    limits = db.execute('select * from paste_limit').fetchmany()
 
-    _visit_limit = limit[1]
-    _file_visited = file[4]
+    # id, name, file, date_time, visited, password
+    files = db.execute('select * from paste_file').fetchmany()
 
-    if datetime.now() >= _file_time+timedelta(hours=_limit_hour):
-        print('yay its worked!')
+    for file, limit in zip(files, limits):
+        _file_time = file[3]
+        _file_time = datetime.strptime(_file_time, "%Y-%m-%d %H:%M:%S.%f")
+        _limit_hour = limit[2]
 
-    if _visit_limit and _file_visited >= _visit_limit:
-        ...
+        _visit_limit = limit[1]
+        _file_visited = file[4]
+
+        if datetime.now() >= _file_time+timedelta(hours=_limit_hour):
+            remove_file(f'./media/{file[2]}')
+            remove_from_db(db, file[0])
+            print(file)
+
+        if _visit_limit and _file_visited >= _visit_limit:
+            remove_file(f'./media/{file[2]}')
+            remove_from_db(db, file[0])
+            print(file)

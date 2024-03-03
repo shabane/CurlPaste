414e2a0f70c72e0eff7947adbfe8d544f08e9643
shabane <m.mohamadshabane@gmail.com>
2024-03-03 10:53:27 +0330

    feat: support view file that protected with password:
        mark corspounding task in .todo.md file

--- paste/views.py
+++ paste/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseNotAllowed, HttpResponseRedirect, FileResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseNotAllowed, HttpResponseRedirect, FileResponse, JsonResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 from curlpaste.settings import MEDIA_ROOT
 from hashlib import md5
@@ -34,6 +34,9 @@ def root(request, idf=None, username=None):
 def file_serv(request, path):
     if request.method == 'GET':
         fli = File.objects.get(file=path)
+        if fli.password is not None:
+            if not request.GET.get('password') == fli.password:
+                return HttpResponseForbidden("try to pass the right password!")
         fli.visited += 1
         fli.save()
         print(fli.visited)

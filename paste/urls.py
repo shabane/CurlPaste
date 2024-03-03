87f36efb723969f41f0aca977fe9439264c10352
shabane <m.mohamadshabane@gmail.com>
2024-03-03 10:57:37 +0330

    patch: add routing without leading /

--- paste/urls.py
+++ paste/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import root, file_serv
+from .views import root, file_serv, ping
 
 
 urlpatterns = [
     path('', root),
     path('<int:idf>/', root),
+    path('<int:idf>', root),
     path('<str:username>/', root),
+    path('<str:username>', root),
     path('file/<str:path>/', file_serv),
+    path('file/<str:path>', file_serv),
 ]

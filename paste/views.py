003eff3c6cf80d1997e47205608275292a724886
shabane <m.mohamadshabane@gmail.com>
2024-03-07 01:11:13 +0330

    patch: return 404 if a file does not exist anymore

--- paste/views.py
+++ paste/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseNotAllowed, HttpResponseRedirect, FileResponse, JsonResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from curlpaste.settings import MEDIA_ROOT
 from hashlib import md5
 import os.path
@@ -33,7 +34,7 @@ def root(request, idf=None, username=None):
 
 def file_serv(request, path):
     if request.method == 'GET':
-        fli = File.objects.get(file=path)
+        fli = get_object_or_404(File, file=path)
         if fli.password is not None:
             if not request.GET.get('password') == fli.password:
                 return HttpResponseForbidden("try to pass the right password!")
